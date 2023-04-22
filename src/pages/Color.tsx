1193fb1d02a997fb45359d042354c3753fa58d99
Frostito <enriquelvis@gmail.com>
2023-04-22 00:49:04 -0600

    Agregando archivos y carpetas

---
+++ src/pages/Color.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+import { 
+    useState 
+    , useEffect
+} from "react";
+
+import { 
+    ColorType
+    , getColors
+    , addColor
+    , deleteColor
+    ,updateColorName
+} from "../services/ColorServices"
+
+
+function Color() {
+
+    const [colors, setColors] = useState<ColorType[]>([]);
+    const [color, setColor] = useState<string>("");   
+
+
+    const changeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setColor(e.target.value);
+    }
+
+    const addColorEvent = async () => {
+        const newColor = await addColor(color);
+        setColors([...colors, newColor]);
+        setColor("");
+    }
+
+    const deleteColorEvent = async (id: number) => {
+        await deleteColor(id);
+        setColors(colors.filter((color) => color.id !== id));
+    }
+    
+    const updateColorNameEvent = async (id: number, name: string) {
+        const updatedColor = await updateColorName({ id, name });
+        console.log(`El color ${updatedColor.id} ha cambiado a ${updatedColor.name}`);
+    }
+    updateColorNameEvent(1, 'Nuevo color');
+    
+        useEffect(() => {
+        async function fetchData() {
+            
+            const x = await getColors();
+            setColors(x);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+        <h1>Colors Management</h1>
+
+            <span>Color: </span>
+            <input 
+                type="text" 
+                placeholder="Type your new color"
+                value={color}
+                onChange={changeInput}
+            />
+            <button
+                disabled={ color.length == 0 }
+                onClick={addColorEvent}
+            >
+                Add
+            </button>
+
+
+            <ul>
+                { colors.map((color) => (
+                    <li key={color.id} >
+                        { color.descripcion }
+                        <button onClick={
+                            () => deleteColorEvent(color.id)
+                        } >
+                            Remove
+                        </button>
+                        <button>
+                            Edit
+                        </button>
+                    </li>
+                    
+                ))}
+            </ul>
+
+        </div>
+    );
+}
+
+export default Color;
